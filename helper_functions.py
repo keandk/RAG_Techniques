a026240357c5c378c9112bd8a7126008b223ed15
Trung Kien Luu <kienluu2703@icloud.com>
2025-03-05 10:00:06 +0700

    RAG branch

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain import PromptTemplate
